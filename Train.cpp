4adecbe69cdbb83e6014918d49cb6e74b14a3b1e
Алексей Кытманов <kytmanov_aleksei@hotmail.com>
2020-08-17 22:59:07 +0300

    Добавление файлов Поезд (Train)

---
+++ Train.cpp
@@ -0,0 +1,10 @@
+#include "Train.h"
+
+Train::Train(int number, string station, int hour, int minute)
+{
+	this->number = number;
+	this->station = station;
+	this->hour = hour;
+	this->minute = minute;
+}
+
---
+++ Train.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Train
+{
+	int number;
+	string station;
+	int hour;
+	int minute;
+
+public:
+	Train(int, string, int, int);
+
+
+};
